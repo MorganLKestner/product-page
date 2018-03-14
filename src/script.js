c2ecd9bcf752362d7409d6928f236ab36c4af579
Morgan Kestner <mkestner@770-MPO-16739.jcrew.com>
2018-03-13 21:50:24 -0400

    reverted css & js

--- src/script.js
+++ src/script.js
@@ -3,4 +3,11 @@ console.log("script is running!!");
 
 // stickybar js plug in 
 
-let sidebar = new StickySidebar('#sidebar', {topSpacing: 20});
+// // let sidebar = new StickySidebar('.sidebar', {topSpacing: 20});
+
+// var stickySidebar = new StickySidebar('.sidebar', {
+//     topSpacing: 20,
+//     bottomSpacing: 20,
+//     containerSelector: '.pdt-container',
+//     innerWrapperSelector: '.sidebar__inner'
+// });
