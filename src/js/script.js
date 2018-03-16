3474f2d61bd5aa2b2628e133d170076a7594789d
Morgan Kestner <mkestner@770-MPO-16739.jcrew.com>
2018-03-16 17:13:53 -0400

    removed DS_Store

--- src/js/script.js
+++ src/js/script.js
@@ -1,50 +1,40 @@
-//  puesdo
-// <----- radio check ---->
-//color choice - check appears on the right - default color is black
+const zoomImageContainer = document.querySelector(".product-zoom-images");
 
-//When user clicks on the radio button the check mark will move to that color with the radio button
+document.querySelectorAll(".product-img").forEach(img => {
+  img.addEventListener("click", event => {
+    zoomImageContainer.classList.remove("hide-show-zoom");
 
-//if the user clicks a color that isn't the previous color with the checkmark
+    const productNumber = event.target.dataset.image;
+    const currentZoomClass = "zoom-img-" + productNumber;
+    const currentZoomElement = document.querySelector("." + currentZoomClass);
+    currentZoomElement.scrollIntoView();
+  });
+});
 
-// then the previous color has the checkmark removed and the check mark is applied to a new color
+zoomImageContainer.addEventListener("click", () => {
+  zoomImageContainer.classList.add("hide-show-zoom");
+});
 
-const zoomImageContainer = document.querySelector('.product-zoom-images')
-
-document.querySelectorAll('.product-img').forEach(img => {
-  img.addEventListener('click', event => {
-    zoomImageContainer.classList.remove('hide-show-zoom')
-
-    const productNumber = event.target.dataset.image
-    const currentZoomClass = 'zoom-img-' + productNumber
-    const currentZoomElement = document.querySelector('.' + currentZoomClass)
-    currentZoomElement.scrollIntoView()
-  })
-})
-
-zoomImageContainer.addEventListener('click', () => {
-  zoomImageContainer.classList.add('hide-show-zoom')
-})
-
-let quantity = 1
+let quantity = 1;
 
 const updateQuantity = () => {
-  document.querySelector('.qty').innerText = 'quantity (' + quantity + ')'
-}
+  document.querySelector(".qty").innerText = "quantity (" + quantity + ")";
+};
 
-document.querySelector('.add-to').addEventListener('click', () => {
+document.querySelector(".add-to").addEventListener("click", () => {
   if (quantity < 15) {
-    quantity++
+    quantity++;
   }
-  updateQuantity()
-})
+  updateQuantity();
+});
 
-document.querySelector('.subtract').addEventListener('click', () => {
+document.querySelector(".subtract").addEventListener("click", () => {
   if (quantity > 1) {
-    quantity--
+    quantity--;
   }
-  updateQuantity()
-})
+  updateQuantity();
+});
 
-$('.carousel').carousel({
+$(".carousel").carousel({
   interval: false
-})
+});
