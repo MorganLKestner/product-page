a3aabd2ab4fe387499d1a2ef11068b1046345762
Morgan Kestner <mkestner@770-MPO-16739.jcrew.com>
2018-03-15 21:50:32 -0400

    added zoom feature, puesdocode, & css

--- src/js/script.js
+++ src/js/script.js
@@ -3,7 +3,7 @@ console.log("script is running!!");
 
 
 //  puesdo 
-
+// <----- radio check ---->
 //color choice - check appears on the right - default color is black
 
 //When user clicks on the radio button the check mark will move to that color with the radio button
@@ -12,33 +12,15 @@ console.log("script is running!!");
 
 // then the previous color has the checkmark removed and the check mark is applied to a new color
 
-const showCheckOnSelect = () => {
- let listener = document.querySelector("#radio1").addEventListener;
 
-    if (document.getElementById('yesCheck').checked) {
-        listener("change", ()=> {
-            alert("checked radio 1");
-        })
-        
-    }
-    else {
-        document.getElementById('ifYes').style.display = 'none';
-    } 
-  }
- 
-  showCheckOnSelect();
 
-  (function() {
-    "use strict";
-  
-      ("change", function() {
-          alert("checked radio 1");
-        
-      });
-  
-      document.querySelector("#radio2").addEventListener("change", function() {
-          alert("checked radio 2");
-      });
-  
-  })();
+
+
+// <----- zoom on click ---->
+
+// on click of the image that image is enlarge
+
+// the click zooms onto the image user is viewing
+
+// on click of the zoomed image that image is back to default state
 
